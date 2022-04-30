62e528bbe347491f3de2be4f62ed6c3b9a4b753d
Paweł Adamczewski <gavron7@gmail.com>
2022-04-30 20:51:29 +0200

    prawie działą

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ class alarm:
         self.list = []
         self.oldtime = 0
         self.position = 0
+        self.old_a = 0
 
     def add(self, pio):
+        if pio> len(self.piority):
+            pio = len(self.piority) - 1
         self.list.append(pio)
         self.find_top_alarm()
 
@@ -41,29 +44,28 @@ class alarm:
         f = alarm['ilosc_piskow_na_sekunde']
         xmax = math.pi * 2
         xmax = int(round(xmax))
-        for i in range(xmax ):
-            wynik =  math.sin(i)
+        for i in range(0, xmax * 10):
+            # generowanie sygnału sinusoidalnego
+            wynik =  math.sin(i * f)
             if wynik > 0:
                 wynik = 1
             elif wynik <= 0:
                 wynik = 0
             out += str(wynik)
-        return out
+        return out + "x"
 
     def run(self):
-        if not self.top() or time.time() - self.oldtime < 2 * math.pi:
+        if not self.top() or time.time() - self.oldtime < 1/(2 * math.pi):
             # czy alarm jest włączony oraz czy upłynął czas od ostatniego wywołania
-            return
+            return self.old_a
         a = self._make_alarm(self.top())
+        self.old_a = a
         return a
 
 a = alarm()
-a.add(4)
-print(a.list)
-print(a.top())
+a.add(0)
 s=time.time()
 while time.time() - s < 3:
     x = a.run()
-    if x:
-        print(len(x), x)
+    print(x)
 
